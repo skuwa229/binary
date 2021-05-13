eb22c28b9f4f035784e7b52dcdbdcd797a8b9716
skuwa229 <skuwa229@gmail.com>
2021-05-13 21:43:41 +0900

    add auto scaling

--- formation/ecs.sh
+++ formation/ecs.sh
@@ -2,8 +2,11 @@
 
 STACK_NAME="go-server-ecs"
 SYSTEM_NAME="go-server"
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+IMAGE_URI=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/${SYSTEM_NAME}
 
 aws cloudformation deploy --stack-name ${STACK_NAME} --template-file ../template/ecs.yml \
+  --capabilities CAPABILITY_IAM \
   --parameter-overrides \
-  SystemName=${SYSTEM_NAME}
-
+  SystemName=${SYSTEM_NAME} \
+  ImageUri=${IMAGE_URI}
