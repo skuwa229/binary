b3a1d71ceffc11916041fd38dd98c935ef8f3b94
skuwa229 <skuwa229@gmail.com>
2021-05-13 20:24:31 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ name="go-server"
 repository_url=$1
 
 eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
-docker build --rm=false -t $name .
+docker build --no-cache --rm=true -t $name .
 docker tag "$name:latest" "$repository_url/$name:latest"
 docker push "$repository_url/$name:latest"
